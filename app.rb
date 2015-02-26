3cc1b2c70f4bf5ca90b11b4cf3993119690627a1
Jay Zheng <jay.j.zheng@gmail.com>
2015-02-25 23:45:21 -0800

    sidekiq configure

--- app.rb
+++ app.rb
@@ -1,4 +1,14 @@
 # encoding: UTF-8
 
 require 'require_all'
+require 'sidekiq'
+
 require_all 'models', 'workers', 'lib', 'extensions'
+
+Sidekiq.configure_client do |config|
+  config.redis = { :size => 1 }
+end
+
+Sidekiq.configure_server do |config|
+  config.redis = { :size => 5 }
+end
