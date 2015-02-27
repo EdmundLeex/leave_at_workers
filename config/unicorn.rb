7127f932bb1f9f9ea2c1725c50d9da32aae58096
Jay Zheng <jay.j.zheng@gmail.com>
2015-02-26 21:12:51 -0800

    update unicorn.rb

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,7 +1,7 @@
 require 'sidekiq'
 
 before_fork do |server, worker|
-   @sidekiq_pid ||= spawn("bundle exec sidekiq -c 6 -r ./app.rb -e production")
+   @sidekiq_pid ||= spawn("bundle exec sidekiq -c 4 -r ./app.rb -e production")
 end
 
 worker_processes 1
@@ -11,6 +11,6 @@ after_fork do |server, worker|
     config.redis = { :size => 1 }
   end
   Sidekiq.configure_server do |config|
-    config.redis = { :size => 5 }
+    config.redis = { :size => 4 }
   end
 end
