3f76471bb157a5e4d1124c134d47ca502a09ded8
Jay Zheng <jay.j.zheng@gmail.com>
2015-02-26 20:50:24 -0800

    add unicorn

--- app.rb
+++ app.rb
@@ -4,11 +4,3 @@ require 'require_all'
 require 'sidekiq'
 
 require_all 'models', 'workers', 'lib', 'extensions'
-
-Sidekiq.configure_client do |config|
-  config.redis = { :size => 1 }
-end
-
-Sidekiq.configure_server do |config|
-  config.redis = { :size => 5 }
-end
