3aedb8adead804d83d79c79d84f4410774c754b9
Jay Zheng <jay.j.zheng@gmail.com>
2015-03-05 00:13:51 -0800

    try heroku again

--- app.rb
+++ app.rb
@@ -1,5 +1,13 @@
 # encoding: UTF-8
 require 'sinatra/activerecord'
+require 'sidekiq'
+
+Sidekiq.configure_client do |config|
+  config.redis = { :size => 1 }
+end
+Sidekiq.configure_server do |config|
+  config.redis = { :size => 1 }
+end
 
 require 'require_all'
 require_all 'workers', 'lib', 'extensions'
--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,16 +1,1 @@
-require 'sidekiq'
-
-before_fork do |server, worker|
-   @sidekiq_pid ||= spawn("bundle exec sidekiq -c 2 -r ./app.rb -e production")
-end
-
 worker_processes 1
-
-after_fork do |server, worker|
-  Sidekiq.configure_client do |config|
-    config.redis = { :size => 1 }
-  end
-  Sidekiq.configure_server do |config|
-    config.redis = { :size => 2 }
-  end
-end
