791f8bc41fef4d2dba982df96dfb8310c7c60391
Jay Zheng <jay.j.zheng@gmail.com>
2015-03-08 11:24:53 -0700

    add authentication for leave_at api client

--- lib/api_clients/leave_at.rb
+++ lib/api_clients/leave_at.rb
@@ -9,16 +9,17 @@ module ApiClients
     def initialize
       super
       @base_url = ENV['LEAVE_AT_API_URL']
+      @headers = { 'Access-Token' => ENV['LEAVE_AT_ACCESS_TOKEN'] }
     end
 
     def get resource, id = nil
       url = id ? resource.to_s + '/' + id.to_s : resource.to_s
-      JSON.parse RestClient.get(@base_url + url)
+      JSON.parse RestClient.get(@base_url + url, @headers)
     end
 
     def update resource, id, params = {}
       url = @base_url + resource.to_s + '/' + id.to_s
-      JSON.parse RestClient.post(url, params.to_json)
+      JSON.parse RestClient.post(url, params.to_json, @headers)
     end
   end
 end
